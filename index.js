519a623738b545de515fdbd9e641b1cc85e5f179
Desiree Zarco <desireeannzarco7@gmail.com>
2023-03-10 11:48:13 -0800

    Adds myfile.md

---
+++ index.js
@@ -0,0 +1,50 @@
+function add(){
+}
+
+function subtract(){
+}
+
+function multiply(){
+}
+
+function divide(){
+}
+
+function add(a, b){
+    return a + b
+}
+
+function subtract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a*b
+}
+
+function divide(a, b){
+    return a/b
+}
+
+function increment(n){
+    return n+=1
+    // n = n + 1
+}
+
+function decrement(n){
+    return n-=1
+    // n = n - 1
+    // n-=1
+}
+
+function makeInt(n){
+    parseInt(n)
+    return n
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
+function makeInt(n){
+    return parseFloat(n, 10)
+}
